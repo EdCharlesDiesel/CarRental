e7c4affd223100f63fe4cc2506dbd1d969f0ac46
Khotso Mokhethi <Mokhetkc@hotmail.com>
2021-09-15 14:31:37 +0200

    .

--- CarRental.Business.Entities/Car.cs
+++ CarRental.Business.Entities/Car.cs
@@ -1,8 +1,21 @@
+using System.Runtime.Serialization;
+
 namespace CarRental.Business.Entities
 {
-
-    public class Car 
+    [DataContract]
+    public class Car
     {
-        
+        [DataMember]
+        public int CarId { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Color { get; set; }
+        [DataMember]
+        public int Year { get; set; }
+        [DataMember]
+        public decimal RentalPrice { get; set; }
+        [DataMember]
+        public bool CurrentlyRented { get; set; }
     }
 }
--- CarRental.Client.Entities/Car.cs
+++ CarRental.Client.Entities/Car.cs
@@ -2,6 +2,49 @@ namespace CarRental.Client.Entities
 {
     public class Car 
     {
-        
+        private int _CarId;
+        string _Description;
+        int _Year;
+        string _Color;
+        decimal _RentalPrice;
+        bool _CurrentlyRented;
+
+        public int CarId
+        {
+            get { return _CarId; }
+            set { _CarId = value; }
+        }        
+
+        public string Description
+        {
+            get { return _Description; }
+            set { _Description = value; }
+        }
+
+        public int Year
+        {
+            get { return _Year; }
+            set { _Year = value; }
+        }
+
+        public string Color
+        {
+            get { return _Color; }
+            set { _Color = value; }
+        }
+
+        public decimal RentalPrice
+        {
+            get { return _RentalPrice; }
+            set { _RentalPrice = value; }
+        }
+
+        public bool CurrentlyRented
+        {
+            get { return _CurrentlyRented; }
+            set { _CurrentlyRented = value; }
+        }
+
+
     }
 }
